8854fabaaf7b2d92d05c86aad40c350115c59f89
Avi Stramer <astramer@gmail.com>
2016-12-11 23:29:28 -0600

    one more small snapshot filename update

--- test/spec/basic.js
+++ test/spec/basic.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('A basic test', function() {
   it('should be possible to go to google', function() {
     browser.url('https://www.google.com');
-    browser.saveScreenshot(process.env.OUTPUT_DIR + '/snapshot-' + Date.now() + '-.png');
+    browser.saveScreenshot(process.env.OUTPUT_DIR + '/snapshot-' + Date.now() + '.png');
     var title = browser.getTitle();
     assert.equal(title, 'Google');
   });
