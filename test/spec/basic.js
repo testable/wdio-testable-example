67c43437b28e04ab9746170a1427801130b7a400
Avi Stramer <astramer@gmail.com>
2017-09-26 08:38:16 -0500

    csv test

--- test/spec/basic.js
+++ test/spec/basic.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var utils = require('testable-utils');
 
 describe('A basic test', function() {
   it('should be possible to go to google', function() {
@@ -6,5 +7,7 @@ describe('A basic test', function() {
     browser.saveScreenshot((process.env.OUTPUT_DIR || '.') + '/snapshot-' + Date.now() + '.png');
     var title = browser.getTitle();
     assert.equal(title, 'Google');
+    var results = browser.testableCsvNext('test-creds.csv');
+    console.log('username: ' + results[0].data.username);
   });
 });
