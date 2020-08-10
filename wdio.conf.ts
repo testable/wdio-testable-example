bea93200cb7adff60fed48293f15c55ac0e2e325
Aamir Mahmood <aamir@aalasolutions.com>
2020-08-11 02:28:50 +0500

    TypeScript Support Added

---
+++ wdio.conf.ts
@@ -0,0 +1,23 @@
+exports.config = {
+  specs: [
+    "test/spec/**"
+  ],
+  capabilities: [
+    {
+      browserName: "chrome"
+    }
+  ],
+  logLevel: "warn",
+  coloredLogs: true,
+  framework: "mocha",
+  waitforTimeout: 15000,
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 15000,
+    require: 'ts-node/register',
+    compilers: [
+      // optional
+      'tsconfig-paths/register'
+    ]
+  }
+};
