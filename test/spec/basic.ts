e29c87fd2247f6fc29de8b579d74adc3d6496df8
Aamir Mahmood <aamir@aalasolutions.com>
2020-08-11 19:24:42 +0500

    cleaned test file

--- test/spec/basic.ts
+++ test/spec/basic.ts
@@ -1,21 +1,14 @@
 import logger from '@wdio/logger';
 import * as assert from 'assert';
-import * as ta from 'testable-utils';
-
 
 const log = logger('test-basic');
 
 describe('A basic test', () => {
-
     it('should have the right title', () => {
         browser.url('https://www.google.com');
         let title = browser.getTitle();
         log.warn('check the title'); // just to demonstrate logging in spec file
         browser.testableScreenshot('HomePage');
-        // expect(browser).toHaveTitle('Google123');
-
-        browser.testableInfo();
         assert.strictEqual(title, 'Google');
     });
-
 });
